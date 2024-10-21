641a114a50d26ad79c9b54bd2ff96c55f42cec75
Sarthak Mundhra <63492158+stkmnd@users.noreply.github.com>
2024-10-20 20:12:23 -0400

    Update Exercise_3.py

--- Exercise_3.py
+++ Exercise_3.py
@@ -3,6 +3,8 @@ class ListNode:
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
     
 class SinglyLinkedList:
     def __init__(self):
@@ -17,6 +19,14 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+        if self.head is None:
+            self.head = ListNode(data)
+        
+        else:
+            curr = self.head
+            while curr.next:
+                curr = curr.next
+            curr.next = ListNode(data)
         
     def find(self, key):
         """
@@ -24,9 +34,32 @@ class SinglyLinkedList:
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        curr = self.head
+        while curr:
+            if curr.data == key:
+                return curr
+            curr = curr.next
+        return None
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        if seld.head is None:
+            return 'Empty list'
+        
+        if self.head.data == key:
+            self.head = self.head.next
+            return
+
+        curr = self.head.next
+        prev = self.head
+
+        while curr:
+            if curr.data == key:
+                prev.next = curr.next
+                return
+            prev = curr
+            curr = curr.next
+        return 'Key not found'
